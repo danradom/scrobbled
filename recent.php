f061a9d1af83a2ed71af2e1f81d08e2562a8be0c
dan radom <danradom@users.noreply.github.com>
2016-04-07 10:41:01 -0600

    Create recent.php

---
+++ recent.php
@@ -0,0 +1,59 @@
+<html>
+<body bgcolor="#669966">
+
+<center>
+<img src="images/solar-bus.jpg">
+</center>
+
+<?php
+$url = "http://ws.audioscrobbler.com/2.0/?method=user.getinfo&user=USERNAME&api_key=API_KEY";
+$xml = simplexml_load_file($url);
+$num = $xml->user->playcount;
+
+echo "Total songs scrobbled:  $num";
+?>
+
+<br><br>
+Last 25 songs <a href="http://en.wikipedia.org/wiki/Audioscrobbler" target="_new">scrobbled<a/>:
+<br><br>
+
+<center>
+
+<table width="80%" border=0>
+<tr>
+<td width="25%"><b>artist</b></td>
+<td width="50%"><b>song</b></td>
+<td width="25%"><b>scrobbled</b></td>
+</tr>
+
+<?php
+
+$url = "http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=USERNSME&api_key=API_KEY&limit=25";
+$xml = simplexml_load_file($url);
+$tracks = $xml->recenttracks->track;
+
+for ($i = 0; $i < 25; $i++) {
+        $trackname = $tracks[$i]->name;
+        $artist = $tracks[$i]->artist;
+        $epoch = (integer) $tracks[$i]->date['uts'];
+
+        $dt = new DateTime("@$epoch");
+        $dt->setTimezone(new DateTimeZone('America/Denver'));
+        $scrobbled = $dt->format('d M Y,  H:i');
+
+        echo "<td width=\"25%\"> $artist</td>
+        <td width=\"50%\"> $trackname</td>
+        <td width=\"25%\"> $scrobbled</td>
+        </tr>";
+}
+
+echo "</table>";
+
+?>
+
+</center>
+<br><br>
+A complete list of what I've been listing to can be found <a href="http://last.fm/user/USERNAME/library" target="_new">here</a>
+
+</body>
+</html>
